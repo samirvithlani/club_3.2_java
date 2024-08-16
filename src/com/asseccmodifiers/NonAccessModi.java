a4c2086b2f92d7a268e7a33382111789ea7acb25
samirvithlani <samir.vithlani83955@gmail.com>
2024-08-16 13:54:17 +0530

    final

---
+++ src/com/asseccmodifiers/NonAccessModi.java
@@ -0,0 +1,35 @@
+package com.asseccmodifiers;
+
+class Parent{
+	
+	public final void test() {
+		System.out.println("test");
+	}
+}
+
+public class NonAccessModi extends Parent{
+	// final class
+//final method
+	// final variable.,.,
+//	public final void test() {
+//		System.out.println("test");
+//	} 
+	//final method can not overide..
+	//final class can not inherited...
+	
+	
+	
+	
+	final int x = 100; //final vairble must intit...
+	
+	public  void demo() {
+		
+		//x= 200;
+	}
+	
+	
+
+	public static void main(String[] args) {
+
+	}
+}
